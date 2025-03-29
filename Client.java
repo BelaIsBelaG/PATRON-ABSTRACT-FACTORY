5f643b6936bebbbf7acb4d9e541eb4abbbf3adb7
Isabela Gonzalez <159276237+BelaIsBelaG@users.noreply.github.com>
2025-03-29 00:44:37 +0000

    Creación de la clase Client

---
+++ Client.java
@@ -0,0 +1,7 @@
+// Client.java
+public class Client {
+    public static void main(String[] args) {
+        AbstractFactory factory = AbstractFactory.getFactory(Architecture.EMBER);
+        CPU cpu = factory.createCPU();
+    }
+}
