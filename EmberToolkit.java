c475ff9ce2696efa8424578b8e8b8518ce09ba37
Isabela Gonzalez <159276237+BelaIsBelaG@users.noreply.github.com>
2025-03-29 00:38:23 +0000

    Creacion de la clase Hija EmberToolkit

---
+++ EmberToolkit.java
@@ -0,0 +1,12 @@
+// EmberToolkit.java
+class EmberToolkit extends AbstractFactory {
+    @Override
+    public CPU createCPU() {
+        return new EmberCPU();
+    }
+
+    @Override
+    public MMU createMMU() {
+        return new EmberMMU();
+    }
+}
