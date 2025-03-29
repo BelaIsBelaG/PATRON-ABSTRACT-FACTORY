2cbe0a9cea6d12666cfe141757d5ab1610730e02
Isabela Gonzalez <159276237+BelaIsBelaG@users.noreply.github.com>
2025-03-29 13:51:42 +0000

    Agregamos un System.out.println que muestra que nuestra cpu ha sido creada correctamente

--- Client.java
+++ Client.java
@@ -3,5 +3,6 @@ public class Client {
     public static void main(String[] args) {
         AbstractFactory factory = AbstractFactory.getFactory(Architecture.EMBER);
         CPU cpu = factory.createCPU();
+        System.out.println("CPU creada: "+cpu);
     }
 }
